81652e6e2f1c9c79af8b8fc1f5744dd21073774a
Saad Idrees <idrees.sa@gmail.com>
2021-04-25 11:35:34 -0400

    Updated to clone the repo on cluster while executing the job

--- grid_scripts/cnn3d_launcher.sh
+++ grid_scripts/cnn3d_launcher.sh
@@ -13,14 +13,16 @@
 module load python/3.6 cuda cudnn
 
 cd $SLURM_TMPDIR
+git clone https://saadidrees:Trespasser987@github.com/saadidrees/RetinaPredictors.git
+cd $SLURM_TMPDIR/RetinaPredictors
 
-cp /home/sidrees/scratch/dynamic_retina/models/cluster_run_model_cnn3d.py $SLURM_TMPDIR/
-cp -R /home/sidrees/scratch/dynamic_retina/models/model $SLURM_TMPDIR/
-
+#cp /home/sidrees/scratch/dynamic_retina/models/cluster_run_model_cnn3d.py $SLURM_TMPDIR/
+#cp -R /home/sidrees/scratch/dynamic_retina/models/model $SLURM_TMPDIR/
 
 virtualenv --no-download  $SLURM_TMPDIR/tensorflow_env
 source $SLURM_TMPDIR/tensorflow_env
 pip install --no-index scipy matplotlib tensorflow_gpu
 
 
-python cluster_run_model_cnn3d.py $expDate --runOnCluster=$runOnCluster --chan1_n=$chan1_n --filt1_size=$filt1_size --filt1_3rdDim=$filt1_3rdDim --chan2_n=$chan2_n --filt2_size=$filt2_size --filt2_3rdDim=$filt2_3rdDim --chan3_n=$chan3_n --filt3_size=$filt3_size --filt3_3rdDim=$filt3_3rdDim --nb_epochs=$nb_epochs --thresh_rr=$thresh_rr --temporal_width=$temporal_width --bz_ms=$bz_ms
+
+python run_model_cnn3d.py $expDate --runOnCluster=$runOnCluster --chan1_n=$chan1_n --filt1_size=$filt1_size --filt1_3rdDim=$filt1_3rdDim --chan2_n=$chan2_n --filt2_size=$filt2_size --filt2_3rdDim=$filt2_3rdDim --chan3_n=$chan3_n --filt3_size=$filt3_size --filt3_3rdDim=$filt3_3rdDim --nb_epochs=$nb_epochs --thresh_rr=$thresh_rr --temporal_width=$temporal_width --bz_ms=$bz_ms
