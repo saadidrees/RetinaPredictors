11274cc427ddc421d19fed65aa089dd7467a24ef
Saad Idrees <idrees.sa@gmail.com>
2021-09-17 16:44:13 -0400

    include parameters for trainable PR

--- grid_scripts/paramFile_gen.py
+++ grid_scripts/paramFile_gen.py
@@ -10,43 +10,45 @@ import csv
 import os
 APPEND_TO_EXISTING = 0
 expDate = 'retina1'
-subfold = '8ms_TRSAMPS'
-datasetName = 'photopic-10000_preproc-added_norm-1_rfac-2'    # ['scotopic','photopic','scotopic_photopic','photopic_shiftedPhotopic']
+subfold = '8ms_trainablePR'
+datasetName = 'photopic-10000'    # ['scotopic','photopic','scotopic_photopic','photopic_shiftedPhotopic']
 path_model_save_base = os.path.join('/home/sidrees/scratch/RetinaPredictors/data',expDate,subfold,datasetName)
 path_dataset = os.path.join('/home/sidrees/scratch/RetinaPredictors/data',expDate,subfold,'datasets')
 name_datasetFile = expDate+'_dataset_train_val_test_'+datasetName+'.h5'       # _dataset_train_val_test_scotopic_photopic
 fname_data_train_val_test = os.path.join(path_dataset,name_datasetFile)
+path_existing_mdl = 0
 
-mdl_name = 'CNN_2D'
+mdl_name = 'PR_CNN3D'
 thresh_rr=0
 temporal_width=120
-bz_ms=10000
+pr_temporal_width=180
+bz_ms=5000
 nb_epochs=200
-TRSAMPS = 10
+TRSAMPS = 0
 
-USE_CHUNKER=0
+USE_CHUNKER=1
 BatchNorm=1
 MaxPool=0
 num_trials=5
 
 chan1_n = np.atleast_1d((13)) #np.array((8,9,10,11,12,13,14,15,16,18,20)) #np.atleast_1d((18)) #np.atleast_1d((18))
 filt1_size = np.atleast_1d((3)) #((1,2,3,4,5,6,7,8,9))
-filt1_3rdDim = np.atleast_1d((0)) #np.atleast_1d((1,10,20,30,40,50,60))
+filt1_3rdDim = np.atleast_1d((20)) #np.atleast_1d((1,10,20,30,40,50,60))
 
 chan2_n = np.atleast_1d((26)) #np.array((0,15,18,20,22,24,25,26,28,30)) #np.atleast_1d((25))     #np.array((8,10,13,15,18,20,22,24,25,26,28,30))
 filt2_size = np.atleast_1d((2))   #((1,2,3,4,5,6,7,8,9))
-filt2_3rdDim = np.atleast_1d((0)) #np.atleast_1d((1,8,10,12,14,18,20,30,40,50))
+filt2_3rdDim = np.atleast_1d((55)) #np.atleast_1d((1,8,10,12,14,18,20,30,40,50))
 
 chan3_n = np.atleast_1d((24)) #np.array((0,15,18,20,22,24,25,26,28,30)) #np.atleast_1d((18))     #np.array((13,15,18,20,22,24,25,26,28,30))
 filt3_size = np.atleast_1d((1))   # ((1,2,3,4,5,6,7,8,9))
-filt3_3rdDim = np.atleast_1d((0))#np.atleast_1d((1,8,10,12,14,18,20,30,40,50))
+filt3_3rdDim = np.atleast_1d((47))#np.atleast_1d((1,8,10,12,14,18,20,30,40,50))
 
 
 image_dim = 10
 image_tempDim = temporal_width
 
 
-csv_header = ['expDate','mdl_name','path_model_save_base','name_datasetFile','thresh_rr','temp_width','bz_ms','nc_epochs','chan1_n','filt1_size','filt1_3rdDim','chan2_n','filt2_size','filt2_3rdDim','chan3_n','filt3_size','filt3_3rdDim','BatchNorm','MaxPool','num_trials','USE_CHUNKER','TRSAMPS']
+csv_header = ['expDate','mdl_name','path_model_save_base','name_datasetFile','path_existing_mdl','thresh_rr','temp_width','pr_temporal_width','bz_ms','nc_epochs','chan1_n','filt1_size','filt1_3rdDim','chan2_n','filt2_size','filt2_3rdDim','chan3_n','filt3_size','filt3_3rdDim','BatchNorm','MaxPool','num_trials','USE_CHUNKER','TRSAMPS']
 params_array = np.zeros((100000,3*3))
 counter = -1
 for cc1 in chan1_n:
@@ -112,14 +114,14 @@ for cc1 in chan1_n:
                                         f3 = 0
                                         d3 = 0
                                         
-                                    if mdl_name=='CNN_3D' and d2==0:
+                                    if (mdl_name=='CNN_3D' or mdl_name[-5:]=='CNN3D') and d2==0:
                                         c2 = 0
                                         f2 = 0
                                         d3 = 0
                                         f3 = 0
                                         d3 = 0
                                         
-                                    if mdl_name=='CNN_3D' and d3==0:
+                                    if (mdl_name=='CNN_3D' or mdl_name[-5:]=='CNN3D') and d3==0:
                                         c3 = 0
                                         f3 = 0
                                         
@@ -127,7 +129,7 @@ for cc1 in chan1_n:
                                     if c2>0 and f2==0:
                                         raise ValueError('Post f2 is 0')
                                     
-                                    if mdl_name=='CNN_3D':
+                                    if (mdl_name=='CNN_3D' or mdl_name[-5:]=='CNN3D'):
                                         conds = np.atleast_1d((d3_out > 1,np.logical_and(c3==0,d2_out>1),np.logical_and(d2_out<1,d1_out>1)))
                                     
                                         if np.any(conds)!=True:   # we want temporal dimension to be flattedned in the last layer
@@ -157,7 +159,7 @@ fname_model = ([])
 for i in range(params_array.shape[0]):
                         
     rgb = params_array[i,:].astype('int').tolist()
-    csv_data = [expDate,mdl_name,path_model_save_base,fname_data_train_val_test,thresh_rr,temporal_width,bz_ms,nb_epochs]
+    csv_data = [expDate,mdl_name,path_model_save_base,fname_data_train_val_test,path_existing_mdl,thresh_rr,temporal_width,pr_temporal_width,bz_ms,nb_epochs]
     csv_data.extend(rgb)
     csv_data.extend([BatchNorm,MaxPool,num_trials,USE_CHUNKER,TRSAMPS])
                
