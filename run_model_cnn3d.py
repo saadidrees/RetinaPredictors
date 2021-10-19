5f8b26113f90a1bc4358f83c26345d43cd0b1d56
Saad Idrees <idrees.sa@gmail.com>
2021-10-19 10:41:48 -0400

    removed replaceDense function

--- run_model_cnn3d.py
+++ run_model_cnn3d.py
@@ -90,7 +90,7 @@ def run_model(expDate,mdl_name,path_model_save_base,fname_data_train_val_test,pa
     from model.data_handler import load_h5Dataset, prepare_data_cnn3d, prepare_data_cnn2d, prepare_data_convLSTM, check_trainVal_contamination, prepare_data_pr_cnn2d
     from model.performance import save_modelPerformance, model_evaluate, model_evaluate_new
     import model.metrics as metrics
-    from model.models import cnn_3d, cnn_2d, cnn_3d_inception, convLSTM, cnn_3d_lstm, cnn_2d_lstm, lstm_cnn_2d, pr_cnn2d, prfr_cnn2d,pr_cnn2d_fixed, pr_cnn3d, prfr_cnn2d_fixed, replaceDense
+    from model.models import cnn_3d, cnn_2d, cnn_3d_inception, convLSTM, cnn_3d_lstm, cnn_2d_lstm, lstm_cnn_2d, pr_cnn2d, prfr_cnn2d,pr_cnn2d_fixed, pr_cnn3d, prfr_cnn2d_fixed
     from model.train_model import train
     from model.load_savedModel import load
     
@@ -269,24 +269,6 @@ def run_model(expDate,mdl_name,path_model_save_base,fname_data_train_val_test,pa
         filt2_3rdDim=0
         filt3_3rdDim=0
         
-    elif mdl_name=='replaceDense_2D':
-        rgb = os.path.split(path_existing_mdl)[-1]
-        mdl_existing = load(os.path.join(path_existing_mdl,rgb))
-        idx_CNN_start = 6
-        
-
-        mdl = replaceDense(mdl_existing,idx_CNN_start,x, n_cells, filt_temporal_width=temporal_width,
-                             chan1_n=chan1_n, filt1_size=filt1_size, chan2_n=chan2_n, filt2_size=filt2_size, chan3_n=chan3_n, filt3_size=filt3_size,
-                             BatchNorm=BatchNorm,MaxPool=MaxPool,BatchNorm_train = BatchNorm_train)
-        
-        fname_model = 'U-%0.2f_T-%03d_C1-%02d-%02d_C2-%02d-%02d_C3-%02d-%02d_BN-%d_MP-%d_TR-%02d' %(thresh_rr,temporal_width,chan1_n,filt1_size,
-                                                                                     chan2_n,filt2_size,
-                                                                                     chan3_n,filt3_size,
-                                                                                     bn_val,mp_val,c_trial)
-        filt1_3rdDim=0
-        filt2_3rdDim=0
-        filt3_3rdDim=0
-
 
     elif mdl_name=='PRFR_CNN2D_fixed': # freds model
         rgb = os.path.split(path_existing_mdl)[-1]
