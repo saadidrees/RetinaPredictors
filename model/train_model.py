307e1bd3cabd19305e87fd66ba8065bf634157b3
Saad Idrees <idrees.sa@gmail.com>
2022-02-22 16:26:18 -0500

    added learningrate scheduler

--- model/train_model.py
+++ model/train_model.py
@@ -15,70 +15,86 @@ from tensorflow.keras.optimizers import Adam
 import model.metrics as metrics
 import numpy as np
 import re
+from tensorflow import keras
+from model.load_savedModel import load
 
+def chunker(data,batch_size,mode='default'):
+    if mode=='predict': # in predict mode no need to do y
+        X = data
+        counter = 0
+        while True:
+            counter = (counter + 1) % X.shape[0]
+            for cbatch in range(0, X.shape[0], batch_size):
+                yield (X[cbatch:(cbatch + batch_size)])
 
-def chunker(data,batch_size,nsamps=0):
-    if nsamps == 0:
+    else:
         X = data.X
         y = data.y
-    else:
-        X = data.X[:nsamps]
-        y = data.y[:nsamps]
-        
-    counter = 0
-    while True:
-        counter = (counter + 1) % X.shape[0]
-        for cbatch in range(0, X.shape[0], batch_size):
-            yield (X[cbatch:(cbatch + batch_size)], y[cbatch:(cbatch + batch_size)])
-
- 
-def train(mdl, data_train, data_val,fname_excel,path_model_base, fname_model, bz=588, nb_epochs=200, validation_batch_size=5000,validation_freq=10,USE_CHUNKER=0,initial_epoch=1):
-    lr = 1e-2
-
-    mdl.compile(loss='poisson', optimizer=Adam(lr), metrics=[metrics.cc, metrics.rmse, metrics.fev])
+            
+        counter = 0
+        while True:
+            counter = (counter + 1) % X.shape[0]
+            for cbatch in range(0, X.shape[0], batch_size):
+                yield (X[cbatch:(cbatch + batch_size)], y[cbatch:(cbatch + batch_size)])
     
-    if initial_epoch>1:
+class CustomCallback(keras.callbacks.Callback):
+
+    def on_epoch_end(self, epoch, logs=None):
+        print("LR - {}".format(self.model.optimizer.learning_rate))
+
+def lr_scheduler(epoch,lr):
+    if epoch == 2:
+        lr = lr/10
+    return lr
+
+# %%
+def train(mdl, data_train, data_val,fname_excel,path_model_base, fname_model, bz=588, nb_epochs=200, validation_batch_size=5000,validation_freq=10,USE_CHUNKER=0,initial_epoch=1,lr=0.001,use_lrscheduler=0):
+    
+    optimizer = Adam(lr)
+    mdl.compile(loss='poisson', optimizer=optimizer, metrics=[metrics.cc, metrics.rmse, metrics.fev],experimental_run_tf_function=False)
+
+    if initial_epoch==0: # 
+        mdl.save(os.path.join(path_model_base,fname_model)) # save model architecture
+
+    else:
         try:
             weight_file = 'weights_'+fname_model+'_epoch-%03d' % initial_epoch
             mdl.load_weights(os.path.join(path_model_base,weight_file))
+
         except:
             weight_file = 'weights_'+fname_model+'_epoch-%03d.h5' % initial_epoch
             mdl.load_weights(os.path.join(path_model_base,weight_file))
+            
+
+        # tf.keras.backend.set_value(mdl.optimizer.learning_rate, lr)
+
+            
+    # mdl.compile(loss='mean_squared_error', optimizer=Adam(lr), metrics=[metrics.cc, metrics.rmse, metrics.fev],experimental_run_tf_function=False)
 
 
     # define model callbacks
     fname_cb = 'weights_'+ fname_model + '_epoch-{epoch:03d}' 
-    cbs = [cb.ModelCheckpoint(os.path.join(path_model_base, fname_cb),save_weights_only=True),
-           cb.ReduceLROnPlateau(min_lr=0, factor=0.2, patience=10),
-           cb.TensorBoard(log_dir=path_model_base, histogram_freq=0, write_grads=False),
-           cb.CSVLogger(os.path.join(path_model_base, fname_excel))]
     
-    # cbs = [cb.ModelCheckpoint(os.path.join(path_model_base, fname_cb)),
-    #        cb.TensorBoard(log_dir=path_model_base, histogram_freq=0, write_grads=False),
-    #        cb.ReduceLROnPlateau(min_lr=0, factor=0.2, patience=10),
-    #        cb.CSVLogger(os.path.join(path_model_base, fname_excel))]
+    cbs = [cb.ModelCheckpoint(os.path.join(path_model_base, fname_cb),save_weights_only=True),
+           cb.TensorBoard(log_dir=path_model_base, histogram_freq=0, write_grads=False),
+           cb.CSVLogger(os.path.join(path_model_base, fname_excel)),
+           cb.ReduceLROnPlateau(monitor='loss',min_lr=1e-6, factor=0.2, patience=2),
+           CustomCallback()]
+   
+    if use_lrscheduler==1:
+        cbs.append(cb.LearningRateScheduler(lr_scheduler))
 
-        
-        # try:
-        #     weight_file = 'weights_'+fname_model+'_epoch-%03d' % initial_epoch
-        # except:
-        #     weight_file = 'weights_'+fname_model+'_epoch-%03d.h5' % initial_epoch
-            
-        # mdl.load_weights(os.path.join(path_model_save,weight_file))
-
-
-    if USE_CHUNKER==0:
+    if USE_CHUNKER==0:  # load all data into gpu ram
         mdl_history = mdl.fit(x=data_train.X, y=data_train.y, batch_size=bz, epochs=nb_epochs,
-                          callbacks=cbs, validation_data=(data_val.X,data_val.y), validation_batch_size=validation_batch_size, validation_freq=validation_freq, shuffle=True, initial_epoch=initial_epoch,use_multiprocessing=True)    # validation_data=(data_test.X,data_test.y)   validation_data=(data_val.X,data_val.y)   validation_batch_size=math.floor(n_val)
+                              callbacks=cbs, validation_data=(data_val.X,data_val.y), validation_freq=validation_freq, shuffle=True, initial_epoch=initial_epoch,use_multiprocessing=True)    # validation_batch_size=validation_batch_size,  validation_data=(data_test.X,data_test.y)   validation_data=(data_val.X,data_val.y)   validation_batch_size=math.floor(n_val)
         
     else:
         batch_size = bz
         steps_per_epoch = int(np.ceil(data_train.X.shape[0]/batch_size))
-        gen = chunker(data_train,batch_size,0)
-        mdl_history = mdl.fit(gen,steps_per_epoch=steps_per_epoch,epochs=nb_epochs,callbacks=cbs, validation_data=(data_val.X,data_val.y), shuffle=True,initial_epoch=initial_epoch,use_multiprocessing=True)    # validation_data=(data_test.X,data_test.y)   validation_data=(data_val.X,data_val.y)   validation_batch_size=math.floor(n_val) # steps_per_epoch=steps_per_epoch
-
-    # mdl_history = mdl.fit(x=data_train.X, y=data_train.y, batch_size=bz, epochs=nb_epochs,
-    #                   callbacks=cbs, shuffle=True)    # validation_data=(data_test.X,data_test.y)   validation_data=(data_val.X,data_val.y)   validation_batch_size=math.floor(n_val)
+        gen_train = chunker(data_train,batch_size)
+        # gen_val = chunker(data_val,validation_batch_size)
+        # mdl_history = mdl.fit(gen_train,steps_per_epoch=steps_per_epoch,epochs=nb_epochs,callbacks=cbs, validation_data=gen_val,shuffle=True,initial_epoch=initial_epoch,use_multiprocessing=True,validation_freq=validation_freq)    # validation_data=(data_test.X,data_test.y)   validation_data=(data_val.X,data_val.y)   validation_batch_size=math.floor(n_val) # steps_per_epoch=steps_per_epoch
+        mdl_history = mdl.fit(gen_train,steps_per_epoch=steps_per_epoch,epochs=nb_epochs,callbacks=cbs, validation_data=(data_val.X,data_val.y),validation_batch_size=validation_batch_size,shuffle=True,initial_epoch=initial_epoch,use_multiprocessing=True,validation_freq=validation_freq)    # validation_data=(data_test.X,data_test.y)   validation_data=(data_val.X,data_val.y)   validation_batch_size=math.floor(n_val) # steps_per_epoch=steps_per_epoch
 
       
     rgb = mdl_history.history
@@ -91,10 +107,6 @@ def train(mdl, data_train, data_val,fname_excel,path_model_base, fname_model, bz
         f.create_dataset(keys[i], data=rgb[keys[i]])
     f.close()
     
-    
-    # fname_model = mdl.name+'_%02d' %training_trial
-    # fname_model = fname_cb_prefix
-    mdl.save(os.path.join(path_model_base,fname_model))
-    
+        
     return mdl_history
 
