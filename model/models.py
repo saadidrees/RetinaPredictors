fixed param PR model for gradient analysis

model/models.py
@@ -24,7 +24,7 @@ def model_definitions():
     """
     
     models_2D = ('CNN_2D','CNN_2D_NORM','CNN_2D_RAT','PRFR_CNN2D','PR_CNN2D','PRFR_CNN2D_RODS','PRFR_CNN2D_RODSTRGAMMA',
-                 'PRFR_CNN2D_RAT',
+                 'PRFR_CNN2D_RAT','PRFR_CNN2D_RAT_NHWC',
                  'BP_CNN2D','BP_CNN2D_PRFRTRAINABLEGAMMA',
                  'BP_CNN2D_MULTIBP','BP_CNN2D_MULTIBP_PRFRTRAINABLEGAMMA','BP_CNN2D_MULTIBP_PRFRTRAINABLEGAMMA_RODS')
     
@@ -939,7 +939,7 @@ class photoreceptor_REIKE(tf.keras.layers.Layer):
         self.hillcoef_scaleFac = tf.Variable(name='hillcoef_scaleFac',initial_value=hillcoef_scaleFac(shape=(1,self.units),dtype='float32'),trainable=False)
         
         hillaffinity_init = tf.keras.initializers.Constant(1.) # 0.5
-        self.hillaffinity = tf.Variable(name='hillaffinity',initial_value=hillaffinity_init(shape=(1,self.units),dtype='float32'),trainable=False)
+        self.hillaffinity = tf.Variable(name='hillaffinity',initial_value=hillaffinity_init(shape=(1,self.units),dtype='float32'),trainable=True)
         hillaffinity_scaleFac = tf.keras.initializers.Constant(1.) 
         self.hillaffinity_scaleFac = tf.Variable(name='hillaffinity_scaleFac',initial_value=hillaffinity_scaleFac(shape=(1,self.units),dtype='float32'),trainable=False)
         
@@ -2176,23 +2176,23 @@ class photoreceptor_RAT_REIKE(tf.keras.layers.Layer):
         return config
 
     def build(self,input_shape):
-        sigma_init = tf.keras.initializers.Constant(2) #1
-        self.sigma = tf.Variable(name='sigma',initial_value=sigma_init(shape=(1,self.units),dtype='float32'),trainable=True)
+        sigma_init = tf.keras.initializers.Constant(1.124) #1
+        self.sigma = tf.Variable(name='sigma',initial_value=sigma_init(shape=(1,self.units),dtype='float32'),trainable=False)
         sigma_scaleFac = tf.keras.initializers.Constant(10.) 
         self.sigma_scaleFac = tf.Variable(name='sigma_scaleFac',initial_value=sigma_scaleFac(shape=(1,self.units),dtype='float32'),trainable=False)
         
-        phi_init = tf.keras.initializers.Constant(2) #1
-        self.phi = tf.Variable(name='phi',initial_value=phi_init(shape=(1,self.units),dtype='float32'),trainable=True)
+        phi_init = tf.keras.initializers.Constant(1.124) #1
+        self.phi = tf.Variable(name='phi',initial_value=phi_init(shape=(1,self.units),dtype='float32'),trainable=False)
         phi_scaleFac = tf.keras.initializers.Constant(10.) 
         self.phi_scaleFac = tf.Variable(name='phi_scaleFac',initial_value=phi_scaleFac(shape=(1,self.units),dtype='float32'),trainable=False)
        
-        eta_init = tf.keras.initializers.Constant(10) #0.2
-        self.eta = tf.Variable(name='eta',initial_value=eta_init(shape=(1,self.units),dtype='float32'),trainable=True)
+        eta_init = tf.keras.initializers.Constant(5.76) #0.2
+        self.eta = tf.Variable(name='eta',initial_value=eta_init(shape=(1,self.units),dtype='float32'),trainable=False)
         eta_scaleFac = tf.keras.initializers.Constant(100.) 
         self.eta_scaleFac = tf.Variable(name='eta_scaleFac',initial_value=eta_scaleFac(shape=(1,self.units),dtype='float32'),trainable=False)
         
-        beta_init = tf.keras.initializers.Constant(1.) #10
-        self.beta = tf.Variable(name='beta',initial_value=beta_init(shape=(1,self.units),dtype='float32'),trainable=True)
+        beta_init = tf.keras.initializers.Constant(0.347) #10
+        self.beta = tf.Variable(name='beta',initial_value=beta_init(shape=(1,self.units),dtype='float32'),trainable=False)
         beta_scaleFac = tf.keras.initializers.Constant(10.) 
         self.beta_scaleFac = tf.Variable(name='beta_scaleFac',initial_value=beta_scaleFac(shape=(1,self.units),dtype='float32'),trainable=False)
 
@@ -2207,18 +2207,18 @@ class photoreceptor_RAT_REIKE(tf.keras.layers.Layer):
         cdark_init = tf.keras.initializers.Constant(1.)
         self.cdark = tf.Variable(name='cdark',initial_value=cdark_init(shape=(1,self.units),dtype='float32'),trainable=False)
         
-        betaSlow_init = tf.keras.initializers.Constant(0.) #tf.keras.initializers.Constant(1.) # 0
-        self.betaSlow = tf.Variable(name='betaSlow',initial_value=betaSlow_init(shape=(1,self.units),dtype='float32'),trainable=True)
+        betaSlow_init = tf.keras.initializers.Constant(0.04) #tf.keras.initializers.Constant(1.) # 0
+        self.betaSlow = tf.Variable(name='betaSlow',initial_value=betaSlow_init(shape=(1,self.units),dtype='float32'),trainable=False)
         betaSlow_scaleFac = tf.keras.initializers.Constant(1.) 
         self.betaSlow_scaleFac = tf.Variable(name='betaSlow_scaleFac',initial_value=betaSlow_scaleFac(shape=(1,self.units),dtype='float32'),trainable=False)
         
-        hillcoef_init = tf.keras.initializers.Constant(0.26) #0.52 #tf.keras.initializers.Constant(1.) # 4
-        self.hillcoef = tf.Variable(name='hillcoef',initial_value=hillcoef_init(shape=(1,self.units),dtype='float32'),trainable=True)
+        hillcoef_init = tf.keras.initializers.Constant(0.4) #0.52 #tf.keras.initializers.Constant(1.) # 4
+        self.hillcoef = tf.Variable(name='hillcoef',initial_value=hillcoef_init(shape=(1,self.units),dtype='float32'),trainable=False)
         hillcoef_scaleFac = tf.keras.initializers.Constant(10.) 
         self.hillcoef_scaleFac = tf.Variable(name='hillcoef_scaleFac',initial_value=hillcoef_scaleFac(shape=(1,self.units),dtype='float32'),trainable=False)
         
-        hillaffinity_init = tf.keras.initializers.Constant(0.05) # 0.26
-        self.hillaffinity = tf.Variable(name='hillaffinity',initial_value=hillaffinity_init(shape=(1,self.units),dtype='float32'),trainable=True)
+        hillaffinity_init = tf.keras.initializers.Constant(0.03326) # 0.26
+        self.hillaffinity = tf.Variable(name='hillaffinity',initial_value=hillaffinity_init(shape=(1,self.units),dtype='float32'),trainable=False)
         hillaffinity_scaleFac = tf.keras.initializers.Constant(10.) 
         self.hillaffinity_scaleFac = tf.Variable(name='hillaffinity_scaleFac',initial_value=hillaffinity_scaleFac(shape=(1,self.units),dtype='float32'),trainable=False)
         
@@ -2233,11 +2233,12 @@ class photoreceptor_RAT_REIKE(tf.keras.layers.Layer):
         gamma_scaleFac = tf.keras.initializers.Constant(10.) 
         self.gamma_scaleFac = tf.Variable(name='gamma_scaleFac',initial_value=gamma_scaleFac(shape=(1,self.units),dtype='float32'),trainable=False)
 
-        gdark_init = tf.keras.initializers.Constant(0.28)    # 20 for rods 
+        gdark_init = tf.keras.initializers.Constant(0.20)    # 20 for rods 
         self.gdark = tf.Variable(name='gdark',initial_value=gdark_init(shape=(1,self.units),dtype='float32'),trainable=False)
         gdark_scaleFac = tf.keras.initializers.Constant(100)    # 28 for cones; 20 for rods 
         self.gdark_scaleFac = tf.Variable(name='gdark_scaleFac',initial_value=gdark_scaleFac(shape=(1,self.units),dtype='float32'),trainable=False)
         
+        # self.timeBin = tf.Variable(name='timeBin',initial_value=tf.constant(16,dtype='int32'),trainable=False)
         self.timeBin = 16 # find a way to fix this in the model  #tf.Variable(name='timeBin',initial_value=timeBin(shape=(1,self.units),dtype='float32'),trainable=False)
 
  
@@ -2294,7 +2295,9 @@ def prfr_cnn2d_rat(inputs,n_out,**kwargs): #(inputs,n_out,filt_temporal_width=12
     y = Reshape((inputs.shape[1],inputs.shape[-2]*inputs.shape[-1]))(inputs)
     y = photoreceptor_RAT_REIKE(units=1)(y)
     y = Reshape((inputs.shape[1],inputs.shape[-2],inputs.shape[-1]))(y)
-    y = y[:,inputs.shape[1]-filt_temporal_width:,:,:]
+    # y = y[:,inputs.shape[1]-filt_temporal_width:,:,:]
+    y = y[:,-filt_temporal_width:,:,:]
+
     
     y = LayerNormalization(epsilon=1e-7)(y)
     
@@ -2345,3 +2348,72 @@ def prfr_cnn2d_rat(inputs,n_out,**kwargs): #(inputs,n_out,filt_temporal_width=12
     mdl_name = 'PRFR_CNN2D_RAT'
     return Model(inputs, outputs, name=mdl_name)
 
+def prfr_cnn2d_rat_nhwc(inputs,n_out,**kwargs): #(inputs,n_out,filt_temporal_width=120,chan1_n=12, filt1_size=13, chan2_n=0, filt2_size=0, chan3_n=0, filt3_size=0, BatchNorm=True, BatchNorm_train=False, MaxPool=False):
+
+    filt_temporal_width = kwargs['filt_temporal_width']
+    chan1_n = kwargs['chan1_n']
+    filt1_size = kwargs['filt1_size']
+    chan2_n = kwargs['chan2_n']
+    filt2_size = kwargs['filt2_size']
+    chan3_n = kwargs['chan3_n']
+    filt3_size = kwargs['filt3_size']
+    BatchNorm = bool(kwargs['BatchNorm'])
+    MaxPool = kwargs['MaxPool']
+    if MaxPool==1:
+        MaxPool=2
+    
+    sigma = 0.1
+    
+    # keras_prLayer = photoreceptor_REIKE(units=1)
+    y = Reshape((inputs.shape[1],inputs.shape[-2]*inputs.shape[-1]))(inputs)
+    y = photoreceptor_RAT_REIKE(units=1)(y)
+    y = Reshape((inputs.shape[1],inputs.shape[-2],inputs.shape[-1]))(y)
+    # y = y[:,inputs.shape[1]-filt_temporal_width:,:,:]
+    y = y[:,-filt_temporal_width:,:,:]
+    y = Permute((2,3,1))(y)  # channels last
+    
+    y = LayerNormalization(epsilon=1e-7)(y)
+    
+    # CNN - first layer
+    y = Conv2D(chan1_n, filt1_size, data_format="channels_last", kernel_regularizer=l2(1e-3),name='CNNs_start')(y)
+    if BatchNorm is True:
+        rgb = y.shape[1:]
+        y = Reshape((rgb))(BatchNormalization(axis=-1)(Flatten()(y)))
+        # y = BatchNormalization(axis=1)(y)   
+        
+    if MaxPool > 1:
+        y = MaxPool2D(MaxPool,data_format='channels_last')(y)
+
+    y = Activation('relu')(GaussianNoise(sigma)(y))
+    
+    # CNN - second layer
+    if chan2_n>0:
+        y = Conv2D(chan2_n, filt2_size, data_format="channels_last", kernel_regularizer=l2(1e-3))(y)
+        if BatchNorm is True:
+            rgb = y.shape[1:]
+            y = Reshape((rgb))(BatchNormalization(axis=-1)(Flatten()(y)))
+            # y = BatchNormalization(axis=1)(y)   
+
+        y = Activation('relu')(GaussianNoise(sigma)(y))
+
+
+    # CNN - third layer
+    if chan3_n>0:
+        y = Conv2D(chan3_n, filt3_size, data_format="channels_last", kernel_regularizer=l2(1e-3))(y)
+        if BatchNorm is True:
+            rgb = y.shape[1:]
+            y = Reshape((rgb))(BatchNormalization(axis=-1)(Flatten()(y)))
+            # y = BatchNormalization(axis=1)(y)   
+        y = Activation('relu')(GaussianNoise(sigma)(y))
+
+    
+    # Dense layer
+    y = Flatten()(y)
+    if BatchNorm is True: 
+        y = BatchNormalization(axis=-1)(y)
+    # y = Dense(n_out, kernel_initializer='normal', kernel_regularizer=l2(1e-3), activity_regularizer=l1(1e-3))(y)
+    y = Dense(n_out, kernel_initializer='normal', kernel_regularizer=l2(1e-3))(y)
+    outputs = Activation('softplus',dtype='float32')(y)
+
+    mdl_name = 'PRFR_CNN2D_RAT'
+    return Model(inputs, outputs, name=mdl_name)