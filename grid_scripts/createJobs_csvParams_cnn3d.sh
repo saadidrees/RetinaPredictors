9f830276e6ffdcf56bcca0c0242d7a548ef37df3
Saad Idrees <idrees.sa@gmail.com>
2023-11-02 12:03:21 -0400

    latest changes for running models on mikes data

--- grid_scripts/createJobs_csvParams_cnn3d.sh
+++ grid_scripts/createJobs_csvParams_cnn3d.sh
@@ -2,7 +2,7 @@
 #int-or-string.sh
 
 
-# updating repo
+#updating repo
 cd from_git
 git fetch --all
 git stash
@@ -47,12 +47,13 @@ MaxPool_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f24) )
 num_trials_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f25) )
 use_chunker_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f26) )
 TRSAMPS_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f27) )
-lr_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f28) )
-lr_scheduler_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f29) )
-idx_unitsToTake_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f30) )
-idxStart_fixedLayers_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f31) )
-idxEnd_fixedLayers_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f32) )
-select_rgctype_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f33) )
+VALSAMPS_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f28) )
+lr_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f29) )
+lr_scheduler_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f30) )
+idx_unitsToTake_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f31) )
+idxStart_fixedLayers_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f32) )
+idxEnd_fixedLayers_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f33) )
+select_rgctype_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f34) )
 
 
 
@@ -99,6 +100,7 @@ do
  typeset -i num_trials=${num_trials_all[i]}
  typeset -i use_chunker=${use_chunker_all[i]}
  typeset -i TRSAMPS=${TRSAMPS_all[i]}
+ typeset -i VALSAMPS=${VALSAMPS_all[i]} 
  lr=${lr_all[i]}
  typeset -i lr_scheduler=${lr_scheduler_all[i]}
  typeset -i idx_unitsToTake=${idx_unitsToTake_all[i]}
@@ -135,6 +137,7 @@ do
  echo "num_trials: $num_trials" 
  echo "USE_CHUNKERS: $use_chunker" 
  echo "TRSAMPS: $TRSAMPS" 
+ echo "VALSAMPS: $VALSAMPS"  
  echo "Learning rate: $lr"
  echo "Learning rate scheduler: $lr_scheduler"
  echo "idxStart_fixedLayers: $idxStart_fixedLayers"
@@ -144,14 +147,14 @@ do
  for ((t=1; t<$num_trials+1; t++));
  do
   typeset -i c_trial=$t
-  JOB_ID=$(sbatch --export=LOG_DIR=$LOG_DIR,expDate=$expDate,mdl_name=$mdl_name,path_model_save_base=$path_model_save_base,fname_data_train_val_test=$fname_data_train_val_test,path_existing_mdl=$path_existing_mdl,runOnCluster=$runOnCluster,chan1_n=$chan1_n,filt1_size=$filt1_size,filt1_3rdDim=$filt1_3rdDim,chan2_n=$chan2_n,filt2_size=$filt2_size,filt2_3rdDim=$filt2_3rdDim,chan3_n=$chan3_n,filt3_size=$filt3_size,filt3_3rdDim=$filt3_3rdDim,chan4_n=$chan4_n,filt4_size=$filt4_size,filt4_3rdDim=$filt4_3rdDim,nb_epochs=$nb_epochs,thresh_rr=$thresh_rr,temporal_width=$temporal_width,pr_temporal_width=$pr_temporal_width,bz_ms=$bz_ms,BatchNorm=$BatchNorm,MaxPool=$MaxPool,c_trial=$c_trial,use_chunker=$use_chunker,TRSAMPS=$TRSAMPS,lr=$lr,lr_scheduler=$lr_scheduler,idx_unitsToTake=$idx_unitsToTake,idxStart_fixedLayers=$idxStart_fixedLayers,idxEnd_fixedLayers=$idxEnd_fixedLayers,select_rgctype=$select_rgctype cnn3d_launcher.sh)
+  JOB_ID=$(sbatch --export=LOG_DIR=$LOG_DIR,expDate=$expDate,mdl_name=$mdl_name,path_model_save_base=$path_model_save_base,fname_data_train_val_test=$fname_data_train_val_test,path_existing_mdl=$path_existing_mdl,runOnCluster=$runOnCluster,chan1_n=$chan1_n,filt1_size=$filt1_size,filt1_3rdDim=$filt1_3rdDim,chan2_n=$chan2_n,filt2_size=$filt2_size,filt2_3rdDim=$filt2_3rdDim,chan3_n=$chan3_n,filt3_size=$filt3_size,filt3_3rdDim=$filt3_3rdDim,chan4_n=$chan4_n,filt4_size=$filt4_size,filt4_3rdDim=$filt4_3rdDim,nb_epochs=$nb_epochs,thresh_rr=$thresh_rr,temporal_width=$temporal_width,pr_temporal_width=$pr_temporal_width,bz_ms=$bz_ms,BatchNorm=$BatchNorm,MaxPool=$MaxPool,c_trial=$c_trial,use_chunker=$use_chunker,TRSAMPS=$TRSAMPS,VALSAMPS=$VALSAMPS,lr=$lr,lr_scheduler=$lr_scheduler,idx_unitsToTake=$idx_unitsToTake,idxStart_fixedLayers=$idxStart_fixedLayers,idxEnd_fixedLayers=$idxEnd_fixedLayers,select_rgctype=$select_rgctype cnn3d_launcher.sh)
  
   echo $JOB_ID
   JOB_ID=$(echo "$JOB_ID" | grep -Eo '[0-9]{1,8}')
  
 #  echo "JOB ID: $A\n\nexpDate: $expDate\nthresh_rr: $thresh_rr\ntemporal_width: $temporal_width\nbz_ms: $bz_ms\nnb_epochs: $nb_epochs\nchan1_n: $chan1_n\nfilt1_size: $filt1_size\nfilt1_3rdDim: $filt1_3rdDim\nfilt1_3rdDim: $filt1_3rdDim\nchan2_n: $chan2_n\nfilt2_size: $filt2_size\nfilt2_3rdDim: $filt2_3rdDim\nchan3_n: $chan3_n\nfilt3_size: $filt3_size\nfilt3_3rdDim: $filt3_3rdDim\nBatchNorm=$BatchNorm\nMaxPool=$MaxPool\nc_trial: $c_trial" > $LOG_DIR/$JOB_ID-out.txt
 
-  printf '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%s\t%d\t%d\t%d\t%d\t%s\t' $JOB_ID $expDate $mdl_name $path_model_save_base $fname_data_train_val_test $path_existing_mdl $thresh_rr $temporal_width $pr_temporal_width $bz_ms $nb_epochs $chan1_n $filt1_size $filt1_3rdDim $chan2_n $filt2_size $filt2_3rdDim $chan3_n $filt3_size $filt3_3rdDim $chan4_n $filt4_size $filt4_3rdDim $BatchNorm $MaxPool $c_trial $use_chunker $TRSAMPS $lr $lr_scheduler $idx_unitsToTake $idxStart_fixedLayers $idxEnd_fixedLayers $select_rgctype | paste -sd '\t' >> job_list.csv
+  printf '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%s\t%d\t%d\t%d\t%d\t%s\t' $JOB_ID $expDate $mdl_name $path_model_save_base $fname_data_train_val_test $path_existing_mdl $thresh_rr $temporal_width $pr_temporal_width $bz_ms $nb_epochs $chan1_n $filt1_size $filt1_3rdDim $chan2_n $filt2_size $filt2_3rdDim $chan3_n $filt3_size $filt3_3rdDim $chan4_n $filt4_size $filt4_3rdDim $BatchNorm $MaxPool $c_trial $use_chunker $TRSAMPS $VALSAMPS $lr $lr_scheduler $idx_unitsToTake $idxStart_fixedLayers $idxEnd_fixedLayers $select_rgctype | paste -sd '\t' >> job_list.csv
  
  done
  
