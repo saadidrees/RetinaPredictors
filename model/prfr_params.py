627384997ca985f36902ac8ac6dbf86c051687c2
Saad Idrees <idrees.sa@gmail.com>
2023-11-27 17:00:37 -0500

    added model params version where beta = 0 and non trainable. To remove feedback

--- model/prfr_params.py
+++ model/prfr_params.py
@@ -116,6 +116,60 @@ def mike_phot_trainable():
     
     return pr_params
 
+def mike_phot_trainable_beta0():
+    pr_params = {}
+    pr_params['sigma'] = 2.2
+    pr_params['sigma_scaleFac'] = 10.
+    pr_params['sigma_trainable'] = True
+    
+    pr_params['phi'] = 2.2
+    pr_params['phi_scaleFac'] = 10.
+    pr_params['phi_trainable'] = True
+
+    pr_params['eta'] = 2.
+    pr_params['eta_scaleFac'] = 1000.
+    pr_params['eta_trainable'] = True
+
+    pr_params['beta'] = 1e-6
+    pr_params['beta_scaleFac'] = 1.
+    pr_params['beta_trainable'] = False
+
+    pr_params['cgmp2cur'] = 0.01
+    pr_params['cgmp2cur_scaleFac'] = 1.
+    pr_params['cgmp2cur_trainable'] = False
+
+    pr_params['cgmphill'] = 3.
+    pr_params['cgmphill_scaleFac'] = 1.
+    pr_params['cgmphill_trainable'] = False
+
+    pr_params['cdark'] = 1.
+    pr_params['cdark_scaleFac'] = 1.
+    pr_params['cdark_trainable'] = False
+
+    pr_params['betaSlow'] = 0.
+    pr_params['betaSlow_scaleFac'] = 1.
+    pr_params['betaSlow_trainable'] = False
+
+    pr_params['hillcoef'] = 4.
+    pr_params['hillcoef_scaleFac'] = 1.
+    pr_params['hillcoef_trainable'] = False
+
+    pr_params['hillaffinity'] = 0.5
+    pr_params['hillaffinity_scaleFac'] = 1.
+    pr_params['hillaffinity_trainable'] = False
+
+    pr_params['gamma'] = 1.
+    pr_params['gamma_scaleFac'] = 10.
+    pr_params['gamma_trainable'] = True
+
+    pr_params['gdark'] = 0.35
+    pr_params['gdark_scaleFac'] = 100.
+    pr_params['gdark_trainable'] = False
+
+    pr_params['timeBin'] = 8
+    
+    return pr_params
+
 
 def mike_phot_trainable_all():
     pr_params = {}
