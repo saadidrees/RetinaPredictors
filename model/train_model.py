21ef48a17c5509df68d6b53c049e680d6b160343
Saad Idrees <idrees.sa@gmail.com>
2024-03-06 12:01:38 -0500

    updated LR scheduler

--- model/train_model.py
+++ model/train_model.py
@@ -58,12 +58,13 @@ def chunker(data,batch_size,mode='default'):
 
 
 def lr_scheduler(epoch,lr):
-    arr_scheduler = np.array([[3,1], #10
+    arr_scheduler = np.array([[3,10], #10
                           [10,1],
                           [20,1],
-                          [30,10], #
+                          [30,10], # 10
+                          [40,1],
                           [50,1],
-                          [100,10],
+                          [90,10],
                           [200,10],
                           [300,1]])
 
