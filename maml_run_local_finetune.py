6787df69464a6f78942629cba1edb5418d7af3e2
Saad Idrees <idrees.sa@gmail.com>
2025-01-06 11:52:09 -0500

    made changes to update writing path

--- maml_run_local_finetune.py
+++ maml_run_local_finetune.py
@@ -24,14 +24,14 @@ base = '/home/saad/data/'
 data_pers = 'ej'
 # pretrained_expDates = ('2018-03-01-4','2018-03-01-0','2018-02-09-5','2007-08-21-5','2008-03-25-4','2012-04-13-0','2013-01-23-6',
 #            '2015-09-23-7','2016-02-17-1','2016-02-17-6','2016-02-17-8','2016-06-13-1','2018-02-06-4')
-pretrained_expDates = 'trainList_20240918a'
+pretrained_expDates = 'trainList_20241115a'
 #('2018-03-01-4','2018-03-01-0','2018-02-09-3')
-ft_expDate = '2018-02-06-4' #'2018-02-06-4' #'2015-10-29-2' #'2018-02-06-4'
+ft_expDate = '2015-10-29-2' #'2018-02-06-4' #'2015-10-29-2' #'2018-02-06-4'
 
-APPROACH = 'maml_summed'
-expFold = APPROACH #'maml2'
+APPROACH = 'metal'
+expFold = 'cluster/'+APPROACH #'maml2'
 
-subFold = 'cluster' 
+subFold = '' 
 dataset = 'CB_mesopic_f4_8ms_sig-4'
 idx_unitsToTake = 0
 select_rgctype=0
@@ -41,16 +41,14 @@ ft_mdl_name = 'CNN2D_FT'
 ft_trainingSamps_dur = 5
 
 
-temporal_width=80
-trainingSamps_dur = -1#1#20 #-1 #0.05 # minutes per dataset
-validationSamps_dur=0.5
+validationSamps_dur=0.05
 testSamps_dur=0.5
 USE_WANDB = 0
 batch_size = 256
-ft_lr_A = 0.1
-ft_nb_epochs_A = 1#2
-ft_lr_B = 5e-2
-ft_nb_epochs_B = 1#18
+ft_lr_A = 0.01
+ft_nb_epochs_A = 5#2
+ft_lr_B = 0.0001
+ft_nb_epochs_B = 15#18
 
 dataset_nameForPaths = pretrained_expDates
 
@@ -64,13 +62,16 @@ path_dataset_base = os.path.join('/home/saad/postdoc_db/analyses/data_'+data_per
 
 # Pre-trained model params
 U = 474
-lr_pretrained = 0.001
+lr_pretrained = 0.0001
 temporal_width=80
-chan1_n=32; filt1_size=3
-chan2_n=32; filt2_size=3
-chan3_n=64; filt3_size=3
-chan4_n=64; filt4_size=3
+chan1_n=64; filt1_size=3
+chan2_n=64; filt2_size=3
+chan3_n=128; filt3_size=3
+chan4_n=128; filt4_size=3
 MaxPool=2
+temporal_width=80
+trainingSamps_dur = -1 #1#20 #-1 #0.05 # minutes per dataset
+
 
 fname_model,dict_params = modelFileName(U=U,P=0,T=temporal_width,CB_n=0,
                                                     C1_n=chan1_n,C1_s=filt1_size,C1_3d=0,
